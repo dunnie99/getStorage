71918571ccda840bb08eafd87facb2edc80697ef
dunnie99 <oluwadunnirachael@gmail.com>
2023-02-25 06:29:13 +0100

    first commit

---
+++ scripts/getStore.ts
@@ -0,0 +1,60 @@
+import { ethers, network } from "hardhat";
+const Web3 = require('web3');
+const web3 = new Web3("https://eth-mainnet.g.alchemy.com/v2/_iScTTmFgRiz_2zUUhQXVT1LAxBBxuSP");
+
+async function main() {
+
+
+    const Uniswap = "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984";
+    const BNBcontract = "0xB8c77482e45F1F44dE1745F52C74426C631bDD52";
+    const Matic = "0x7D1AfA7B718fb893dB30A3aBc0Cfc608AaCfeBB0";
+    const ShibaInu = "0x95aD61b0a150d79219dCF64E1E6Cc01f0B64C4cE";
+    const DaiStable ="0x6B175474E89094C44Da98b954EedeAC495271d0F";
+
+    // Reading from Uniswap
+    const Info = await web3.eth.getStorageAt(Uniswap, 1);
+    console.log(`getUniswapStorageAt 1st position ${Info}`);
+
+
+    // Reading from BNB contract
+    const Info1 = await web3.eth.getStorageAt(BNBcontract, 2);
+    console.log(`getBNBStorageAt 2nd position ${Info1}`);
+    //console.log('StorageAt 2nd postion: ' + web3.toDecimal(Info1));
+
+
+    // Reading from Matic
+    const Info2 = await web3.eth.getStorageAt(Matic, 0);
+    console.log(`getMaticSTorageAt 0 position ${Info2}`);
+
+
+    // Reading from Shiba
+    const Info3 = await web3.eth.getStorageAt(ShibaInu, 4);
+    console.log(`getShibaStorageAt 4th position ${Info3}`);
+
+    // Reading from Dai
+    const Info4 = await web3.eth.getStorageAt(DaiStable, 3);
+    console.log(`getDaiStorageAt 3rd position ${Info4}`);
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
+
